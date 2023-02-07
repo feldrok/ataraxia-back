2bcb031b78d69960da76c97a50395195243662bd
Ariel Gonzalez Ayala <119444661+arielgonzalezayala@users.noreply.github.com>
2023-02-07 16:57:01 -0300

    Backend ready (God willing)

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ import express from 'express'
 import users from './users.route.js'
 const router = express.Router()
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
     res.send('Server ready!')
 })
--- routes/users.route.js
+++ routes/users.route.js
@@ -1,11 +1,17 @@
 import controller from '../controllers/users.controller.js'
 import { createSchema } from '../schemas/users.schema.js'
 import express from 'express'
+import passport from 'passport'
 import validator from '../middlewares/validator.js'
+
 const router = express.Router()
 
-const { signup } = controller
+const { signup, verifyCode, signin, signintoken, signout } = controller
 
 router.post('/signup', validator(createSchema),signup)
+router.get("/verify_code", verifyCode)
+router.post("signin", signin)
+router.post("/token", passport.authenticate("jwt", {session: false}), signintoken)
+router.put("/signout", passport.authenticate("jwt", { session: false }), signout)
 
 export default router
