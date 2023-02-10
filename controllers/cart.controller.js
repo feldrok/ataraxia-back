b70b8115de9364fe36eb6b358782559d0863bdc7
feldrok <matias@moravalero.cl>
2023-02-10 13:50:37 -0300

    cart working

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -47,6 +47,86 @@ const controller = {
             next(error)
         }
     },
+    add_product_to_cart: async (req, res, next) => {
+        try {
+            const { id } = req.params
+            const { product_id, quantity } = req.body
+            let cart = await Cart.find({ user_id: id }).populate(
+                'products.product_id'
+            )
+            const cartProducts = cart[0].products.map(
+                (product) => product.product_id
+            )
+            const updatedProduct = cartProducts.find(
+                (product) => product._id.toString() === product_id
+            )
+            if (updatedProduct) {
+                let cart = await Cart.find({ user_id: id }).populate(
+                    'products.product_id'
+                )
+                const updatedCart = cart[0].products.map((product) => {
+                    if (product.product_id._id.toString() === product_id) {
+                        return {
+                            product_id: updatedProduct._id,
+                            quantity: product.quantity + quantity,
+                        }
+                    } else {
+                        return {
+                            product_id: product.product_id._id,
+                            quantity: product.quantity,
+                        }
+                    }
+                })
+                cart = await Cart.findOneAndUpdate(
+                    { user_id: id },
+                    { $set: { products: updatedCart } },
+                    { new: true }
+                )
+                cart = await Cart.find({ user_id: id }).populate(
+                    'products.product_id'
+                )
+                const productsPrice = await (
+                    await Product.find({ _id: { $in: cartProducts } })
+                ).map((product) => product.price)
+                const productsQuantity = cart[0].products.map(
+                    (product) => product.quantity
+                )
+                const total = productsPrice.reduce(
+                    (total, price, index) =>
+                        total + price * productsQuantity[index],
+                    0
+                )
+                cart = await Cart.findOneAndUpdate(
+                    { user_id: id },
+                    { $set: { products: updatedCart, total_price: total } },
+                    { new: true }
+                )
+                req.body.succes = true
+                req.body.sc = 200
+                req.body.data = cart
+                return defaultResponse(req, res)
+            } else {
+                cart = await Cart.findOneAndUpdate(
+                    { user_id: id },
+                    {
+                        $push: {
+                            products: {
+                                product_id: product_id,
+                                quantity: quantity,
+                            },
+                        },
+                    },
+                    { new: true }
+                )
+                req.body.succes = true
+                req.body.sc = 200
+                req.body.data = cart
+                return defaultResponse(req, res)
+            }
+        } catch (error) {
+            next(error)
+        }
+    },
     update_cart: async (req, res, next) => {
         try {
             const { id } = req.params
