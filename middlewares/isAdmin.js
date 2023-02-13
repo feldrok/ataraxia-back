3d622eb0d3d58419b12385c03d4a2137cc735417
Gustavo_Espinoza <gustavo.espinoza.aliste@gmail.com>
2023-02-13 14:03:15 -0300

    modificando controlador admin Panel

--- middlewares/isAdmin.js
+++ middlewares/isAdmin.js
@@ -1,14 +1,21 @@
 import defaultResponse from '../config/defaultResponse.js'
+import { User } from '../models/User.model.js'
 
-async function isAdmin(req, res, next) {
-    if (req.is_admin) {
-        return next()
+const isAdmin = async (req, res, next) => {
+    try {
+        const user = await User.findOne({ is_admin: true })
+        if (user.is_admin == false) {
+            req.body.success = false
+            req.body.message = 'You are not an admin'
+            return res
+                .status(defaultResponse.status)
+                .json(defaultResponse.message)
+        }
+        req.user = user
+        next()
+        console.log(user.is_admin)
+    } catch (error) {
+        next(error)
     }
-    console.log(req.is_admin)
-    req.body.success = false
-    req.body.sc = 401
-    req.body.data = 'you are not allowed'
-    return defaultResponse(req, res)
 }
-
 export default isAdmin
